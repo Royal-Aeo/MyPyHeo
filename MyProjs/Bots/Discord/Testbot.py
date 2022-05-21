283328685e52522d4d7d0e6ff5b7576144ec5674
ROYAL_AEO <80609498+Royals-Aeo-Gamer@users.noreply.github.com>
2022-05-21 10:06:35 +0530

    Update Testbot.py

--- MyProjs/Bots/Discord/Testbot.py
+++ MyProjs/Bots/Discord/Testbot.py
@@ -1,14 +1,17 @@
 import discord
-
+import re
+I = re.compile(r"channel:#[a-z0-9]")
+Text = re.compile(r'text:"[a-z0-9]")
 clint = discord.Client()
 print("starts!")
 
 @clint.event
 async def on_message(message):
-    #if msg.author == msg.user:
-        #return
-    if message.content.startswith("&Testbot"):
-      await message.channel.send("Yeah! I am lisining my baby!")
+    Msg = message.content
+    if Msg.startswith("/testbot-sendit"):
+      Msg.remove("/testbot-sendit ")
+      Ch = re.search(I,Msg)
+      await message.Ch.send(re.search(Text,Msg))
 
 
 clint.run("OTc1MzcyOTYzMjU3OTE3NDQw.G3S7s2.P97pGnQBCVVNxnHQqLseiLgi1MnSjnXbn6suMc")
